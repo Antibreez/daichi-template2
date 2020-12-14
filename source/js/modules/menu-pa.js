c3740992b2f57c9ceaf1d1be8060144c4a05936a
Anton Kiyan <kiyananton@gmail.com>
2020-12-15 01:19:35 +0300

    Добавляет карточку подписки

--- source/js/modules/menu-pa.js
+++ source/js/modules/menu-pa.js
@@ -1,14 +1,16 @@
 (() => {
-  const swiper = new Swiper('.menu-pa__container', {
-    freeMode: true,
-    slidesPerView: 'auto',
-    breakpoints: {
-      // when window width is >= 640px
-      1279: {
-        freeMode: true,
-        slidesPerView: 'auto',
-        allowTouchMove: false
+  $(window).load(function () {
+    const swiper = new Swiper('.menu-pa__container', {
+      freeMode: true,
+      slidesPerView: 'auto',
+      breakpoints: {
+
+        1280: {
+          freeMode: true,
+          slidesPerView: 'auto',
+          allowTouchMove: false
+        }
       }
-    }
-  })
+    });
+  });
 })();
