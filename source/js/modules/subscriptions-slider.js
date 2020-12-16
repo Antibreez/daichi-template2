75660c5cb96f8a273c53c6a97b3e08ed420b7cb5
Anton Kiyan <kiyananton@gmail.com>
2020-12-16 20:18:48 +0300

    Изменяет версию слайдера

--- source/js/modules/subscriptions-slider.js
+++ source/js/modules/subscriptions-slider.js
@@ -6,7 +6,9 @@
       return;
     }
 
-    const swiper = new Swiper($slider, {
+    console.log($slider);
+    const swiper = new Swiper('.subscriptions__slider', {
+      mousewheel: true,
       slidesPerView: 'auto',
       freeMode: true,
       watchOverflow: true,
@@ -15,28 +17,11 @@
         nextEl: '.subscriptions__nav-next',
         prevEl: '.subscriptions__nav-prev',
       },
-      mousewheel: true,
-
-      // freeMode: false,
-      // slidesPerView: 2,
-      // spaceBetween: 15,
-      // watchOverflow: true,
-      // mousewheel: true,
-      // navigation: {
-      //   nextEl: '.subscriptions__nav-next',
-      //   prevEl: '.subscriptions__nav-prev',
-      // },
 
       breakpoints: {
         1280: {
           slidesPerView: 2,
           freeMode: false,
-          spaceBetween: 15,
-          navigation: {
-            nextEl: '.subscriptions__nav-next',
-            prevEl: '.subscriptions__nav-prev',
-          },
-          mousewheel: true,
         }
       }
     });
