8ae57f38148477e0dbf3ea2e814309729db90caf
Anton Kiyan <kiyananton@gmail.com>
2020-12-16 19:58:52 +0300

    Добавляет баннер в личный кабинет

--- source/js/modules/subscriptions-slider.js
+++ source/js/modules/subscriptions-slider.js
@@ -1,22 +1,44 @@
 (() => {
   $(window).load(function () {
-    const swiper = new Swiper('.subscriptions__slider', {
-      slidesPerView: 2,
-      spaceBetween: 15,
+    const $slider = $('.subscriptions__slider');
+
+    if (!$slider) {
+      return;
+    }
+
+    const swiper = new Swiper($slider, {
+      slidesPerView: 'auto',
+      freeMode: true,
       watchOverflow: true,
-      mousewheel: true,
+      spaceBetween: 15,
       navigation: {
         nextEl: '.subscriptions__nav-next',
         prevEl: '.subscriptions__nav-prev',
       },
-      // breakpoints: {
+      mousewheel: true,
 
-      //   1280: {
-      //     freeMode: true,
-      //     slidesPerView: 'auto',
-      //     allowTouchMove: false
-      //   }
-      // }
+      // freeMode: false,
+      // slidesPerView: 2,
+      // spaceBetween: 15,
+      // watchOverflow: true,
+      // mousewheel: true,
+      // navigation: {
+      //   nextEl: '.subscriptions__nav-next',
+      //   prevEl: '.subscriptions__nav-prev',
+      // },
+
+      breakpoints: {
+        1280: {
+          slidesPerView: 2,
+          freeMode: false,
+          spaceBetween: 15,
+          navigation: {
+            nextEl: '.subscriptions__nav-next',
+            prevEl: '.subscriptions__nav-prev',
+          },
+          mousewheel: true,
+        }
+      }
     });
   })
 })();
