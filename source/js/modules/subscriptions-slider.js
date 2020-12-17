a111de5c1a9c8ecc6d6c918ff32ac538477f8f1b
Anton Kiyan <kiyananton@gmail.com>
2020-12-17 17:36:03 +0300

    Добавляет адаптивность

--- source/js/modules/subscriptions-slider.js
+++ source/js/modules/subscriptions-slider.js
@@ -12,14 +12,18 @@
       slidesPerView: 'auto',
       freeMode: true,
       watchOverflow: true,
-      spaceBetween: 15,
+      spaceBetween: 5,
       navigation: {
         nextEl: '.subscriptions__nav-next',
         prevEl: '.subscriptions__nav-prev',
       },
 
       breakpoints: {
+        768: {
+          spaceBetween: 15,
+        },
         1280: {
+          spaceBetween: 15,
           slidesPerView: 2,
           freeMode: false,
         }
